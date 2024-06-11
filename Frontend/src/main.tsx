06d0c8cdc9c054332cc2e33b33b69bf239df0f6d
TalalMajeed <m.talal.majeed@gmail.com>
2024-06-11 16:48:50 +0500

    V.1.2 Router & SCSS Configuration

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    useNavigate,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "./index.scss";
+import Login from "./pages/Login";
+
+const IndexPage: Function = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate("/login");
+    }, []);
+    return <></>;
+};
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <IndexPage />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <RouterProvider router={router} />
+);
